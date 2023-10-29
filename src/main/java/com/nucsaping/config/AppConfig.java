e05854f6f0a330bce73c5ec43304bd33afc6c47a
Muhammad Irfan <mirfan.duri@gmail.com>
2023-10-29 22:51:06 +0700

    Add comments

--- src/main/java/com/nucsaping/config/AppConfig.java
+++ src/main/java/com/nucsaping/config/AppConfig.java
@@ -15,6 +15,13 @@ public class AppConfig {
         return new LettuceConnectionFactory();
     }
 
+    /**
+     * The first type parameter, String, specifies the data type for keys.
+     * This means that Redis keys are expected to be of type String.
+     *
+     * The second type parameter, Employee, specifies the data type for values. In this case,
+     * it indicates that the values stored and retrieved using this RedisTemplate are of type Employee.
+     */
     @Bean
     public RedisTemplate<String, Employee> redisTemplate() {
         RedisTemplate<String, Employee> employeeRedisTemplate = new RedisTemplate<String, Employee>();
